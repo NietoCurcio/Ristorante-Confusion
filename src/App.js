2abb1d8fc1bb4f7d94ed6c642b0cc056f10db49f
NietoCurcio <felipe_nieto010@hotmail.com>
2020-12-24 18:23:51 -0300

    configuring react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { Navbar } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar bg='dark' variant='dark'>
+        <Navbar.Brand href='#home'>
+          <img
+            alt=''
+            src='/logo.svg'
+            width='30'
+            height='30'
+            className='d-inline-block align-top'
+          />{' '}
+          React Bootstrap
+        </Navbar.Brand>
+      </Navbar>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
