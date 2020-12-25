98e6af8e4cb604dd076dd66bd416cbc3fb1c3ce7
NietoCurcio <felipe_nieto010@hotmail.com>
2020-12-25 00:57:53 -0300

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { Navbar } from 'react-bootstrap'
 import MenuComponent from './components/MenuComponent'
-import 'App.css'
+// import 'App.css'
+// Note that to use the className, we have to import the file
+import { DISHES } from './shared/dishes'
+import { useState } from 'react'
 
 function App() {
+  const [dishes, setDishes] = useState(DISHES)
+
   return (
-    <div className='App'>
+    <div>
       <Navbar bg='dark' variant='dark'>
         <Navbar.Brand href='#home'>Restorante Con Fusion</Navbar.Brand>
       </Navbar>
-      <MenuComponent />
+      <MenuComponent dishes={dishes} />
     </div>
   )
 }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,72 +1,48 @@
 import React, { useState } from 'react'
-import { Media } from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
 
-const MenuComponent = () => {
-  const [dishes, setDishes] = useState([
-    {
-      id: 0,
-      name: 'Uthappizza',
-      image: 'assets/images/uthappizza.png',
-      category: 'mains',
-      label: 'Hot',
-      price: '4.99',
-      description:
-        'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-    },
-    {
-      id: 1,
-      name: 'Zucchipakoda',
-      image: 'assets/images/zucchipakoda.png',
-      category: 'appetizer',
-      label: '',
-      price: '1.99',
-      description:
-        'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-    },
-    {
-      id: 2,
-      name: 'Vadonut',
-      image: 'assets/images/vadonut.png',
-      category: 'appetizer',
-      label: 'New',
-      price: '1.99',
-      description:
-        'A quintessential ConFusion experience, is it a vada or is it a donut?',
-    },
-    {
-      id: 3,
-      name: 'ElaiCheese Cake',
-      image: 'assets/images/elaicheesecake.png',
-      category: 'dessert',
-      label: '',
-      price: '2.99',
-      description:
-        'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-    },
-  ])
+const MenuComponent = (props) => {
+  const [selectedDish, setSelectedDishe] = useState({})
 
-  const menu = dishes
+  const onDishSelected = (e, dish) => {
+    setSelectedDishe(dish)
+  }
 
+  const renderDish = (dish) => {
+    if (dish) {
+      return (
+        <Card>
+          <Card.Img variant='top' src={dish.image} alt={dish.name} />
+          <Card.Body>
+            <Card.Title>{dish.name}</Card.Title>
+            <Card.Text>{dish.description}</Card.Text>
+          </Card.Body>
+        </Card>
+      )
+    } else {
+      return <div></div>
+    }
+  }
+
+  const menu = props.dishes
   return (
     <div className='container'>
       <div className='row'>
-        <ul className='list-unstyled'>
-          {menu.map((dish) => {
-            return (
-              <div key={dish.id} className='col-12 mt-5'>
-                {/* React requires a key, to use that property when a item is added, updated or removed from the list */}
-                <Media as='li'>
-                  <img src={dish.image} alt={dish.name} />
-                  <Media.Body className='ml-5'>
-                    <h5>{dish.name}</h5>
-                    <p>{dish.description}</p>
-                  </Media.Body>
-                </Media>
-              </div>
-            )
-          })}
-        </ul>
+        {menu.map((dish, index) => {
+          return (
+            <div key={dish.id} className='col-4'>
+              {/* React requires a key, to use that property when a item is added, updated or removed from the list */}
+              <Card onClick={(e) => onDishSelected(e, dish)}>
+                <Card.Img variant='top' src={dish.image} alt={dish.name} />
+                <Card.Body className='ml-5'>
+                  <Card.Title>{dish.name}</Card.Title>
+                </Card.Body>
+              </Card>
+            </div>
+          )
+        })}
       </div>
+      <div className='row'>{renderDish(selectedDish)}</div>
     </div>
   )
 }
